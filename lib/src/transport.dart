dbb43ca6c2dc81dec6e3f671a41fd54f2d7a75c7
Hiroshi Horie <me@hiroshi.app>
2021-09-03 14:38:07 +0900

    Safer `PCTransport`
    Update transport.dart

--- lib/src/transport.dart
+++ lib/src/transport.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:collection/collection.dart';
 
 /// a wrapper around PeerConnection
 class PCTransport {
   //
-  RTCPeerConnection pc;
-  List<RTCIceCandidate> pendingCandidates = [];
+  final RTCPeerConnection pc;
   bool restartingIce = false;
+  final List<RTCIceCandidate> _pendingCandidates = [];
 
   PCTransport(this.pc);
 
@@ -33,28 +32,40 @@ class PCTransport {
   }
 
   Future<void> setRemoteDescription(RTCSessionDescription sd) async {
+    //
     await pc.setRemoteDescription(sd);
 
-    await Future.forEach<RTCIceCandidate>(pendingCandidates, (candidate) async {
+    await Future.forEach<RTCIceCandidate>(_pendingCandidates, (candidate) async {
       await pc.addCandidate(candidate);
     });
 
-    pendingCandidates.clear();
+    _pendingCandidates.clear();
     restartingIce = false;
   }
 
   Future<void> addIceCandidate(RTCIceCandidate candidate) async {
+    //
     final desc = await getRemoteDescription();
+
     if (desc != null && !restartingIce) {
-      return pc.addCandidate(candidate);
+      await pc.addCandidate(candidate);
+      return;
     }
-    pendingCandidates.add(candidate);
+
+    _pendingCandidates.add(candidate);
   }
 
   Future<RTCSessionDescription?> getRemoteDescription() async {
-    if (pc.iceConnectionState == null) {
-      return null;
+    //
+    // Checking agains null doesn't work as intended
+    // if (pc.iceConnectionState == null) return null;
+    //
+    try {
+      final result = await pc.getRemoteDescription();
+      print('pc.getRemoteDescription $result');
+      return result;
+    } catch (_) {
+      print('pc.getRemoteDescription did throw: $_');
     }
-    return pc.getRemoteDescription();
   }
 }
