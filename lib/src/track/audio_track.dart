3f4366ce60e26bb6e88fad2c4a94d191aff68c6b
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-09-28 02:32:07 +0900

    keep track of local / remote audio tracks

--- lib/src/track/audio_track.dart
+++ lib/src/track/audio_track.dart
@@ -6,16 +6,29 @@ import 'local_audio_track.dart';
 import 'track.dart';
 
 class AudioTrack extends Track {
+  static int localCount = 0;
+  static int remoteCount = 0;
+
   rtc.MediaStream? mediaStream;
 
-  AudioTrack(String name, rtc.MediaStreamTrack track, this.mediaStream)
-      : super(lk_models.TrackType.AUDIO, name, track);
+  AudioTrack(
+    String name,
+    rtc.MediaStreamTrack track,
+    this.mediaStream,
+  ) : super(
+          lk_models.TrackType.AUDIO,
+          name,
+          track,
+        );
 
   /// Start playing audio track. On web platform, create an audio element and
   /// start playback
   void start() {
-    if (this is! LocalAudioTrack) {
+    if (this is LocalAudioTrack) {
+      localCount++;
+    } else if (this is! LocalAudioTrack) {
       audio.startAudio(getCid(), mediaStreamTrack);
+      remoteCount++;
     }
   }
 
@@ -24,6 +37,13 @@ class AudioTrack extends Track {
     await mediaStream?.dispose();
     mediaStream = null;
     audio.stopAudio(getCid());
+
+    if (this is LocalAudioTrack) {
+      localCount--;
+    } else if (this is! LocalAudioTrack) {
+      remoteCount--;
+    }
+
     await super.stop();
   }
 }
