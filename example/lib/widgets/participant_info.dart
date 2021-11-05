c78a883c83c5fd69107bbbe756d40e93f3d3b597
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-06 06:20:30 +0900

    implement example

--- example/lib/widgets/participant_info.dart
+++ example/lib/widgets/participant_info.dart
@@ -1,14 +1,17 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:livekit_client/livekit_client.dart';
 
 class ParticipantInfoWidget extends StatelessWidget {
   //
   final String? title;
   final bool audioAvailable;
+  final ConnectionQuality connectionQuality;
 
   const ParticipantInfoWidget({
     this.title,
     this.audioAvailable = true,
+    this.connectionQuality = ConnectionQuality.unknown,
     Key? key,
   }) : super(key: key);
 
@@ -38,6 +41,20 @@ class ParticipantInfoWidget extends StatelessWidget {
                 size: 16,
               ),
             ),
+            if (connectionQuality != ConnectionQuality.unknown)
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: Icon(
+                  connectionQuality == ConnectionQuality.poor
+                      ? EvaIcons.wifiOffOutline
+                      : EvaIcons.wifi,
+                  color: {
+                    ConnectionQuality.excellent: Colors.green,
+                    ConnectionQuality.good: Colors.orange,
+                    ConnectionQuality.poor: Colors.red,
+                  }[connectionQuality],
+                ),
+              ),
           ],
         ),
       );
