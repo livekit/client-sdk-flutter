7b15d6ee57307bf0bb3a577a98eeeeac10c0f70a
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-11 00:10:35 +0900

    typo

--- lib/src/participant/local_participant.dart
+++ lib/src/participant/local_participant.dart
@@ -234,7 +234,7 @@ class LocalParticipant extends Participant {
 }
 
 extension LocalParticipantTrackSourceExt on LocalParticipant {
-  Future<void> setCamercaEnabled(bool enabled) async {
+  Future<void> setCameraEnabled(bool enabled) async {
     return setSourceEnabled(TrackSource.camera, enabled);
   }
 
