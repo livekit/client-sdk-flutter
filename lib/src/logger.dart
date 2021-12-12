93d0bc0c1904d5aee3acba6d07156a8fe90a7bf0
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-12-12 18:17:48 +0700

    logger docs

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,3 +1,10 @@
 import 'package:logging/logging.dart';
-
+/// Logger used for the entire SDK. To output logs, configure the logger at the
+/// entry point of your app. Adjust the logger output level to your needs.
+/// A typical configuration would look like the following:
+/// 
+/// Logger.root.level = Level.FINE;
+/// Logger.root.onRecord.listen((record) {
+///   print('${record.level.name}: ${record.message}');
+/// });
 final logger = Logger('livekit');
