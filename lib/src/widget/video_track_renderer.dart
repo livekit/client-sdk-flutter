2e7347aabaa9c9fc42ef5a65b18255b8faed5022
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-12-29 02:26:57 +0700

    use Key for a reliable id

--- lib/src/widget/video_track_renderer.dart
+++ lib/src/widget/video_track_renderer.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
+import 'package:uuid/uuid.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 import '../events.dart';
-import '../extensions.dart';
 import '../internal/events.dart';
-import '../logger.dart';
 import '../managers/event.dart';
 import '../track/local/local.dart';
 import '../track/local/video.dart';
@@ -15,12 +13,13 @@ import '../track/local/video.dart';
 class VideoTrackRenderer extends StatefulWidget {
   final VideoTrack track;
   final rtc.RTCVideoViewObjectFit fit;
+  static const uuid = Uuid();
 
-  const VideoTrackRenderer(
+  VideoTrackRenderer(
     this.track, {
     this.fit = rtc.RTCVideoViewObjectFit.RTCVideoViewObjectFitContain,
     Key? key,
-  }) : super(key: key);
+  }) : super(key: key ?? ValueKey('VideoView-${uuid.v4()}'));
 
   @override
   State<StatefulWidget> createState() => _VideoTrackRendererState();
@@ -31,13 +30,9 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
   bool _rendererReady = false;
   EventsListener<TrackEvent>? _listener;
 
-  Key get _keyForVisibilityDetector =>
-      ValueKey('${widget.objectId}-VisibilityDetector');
-
   @override
   void initState() {
     super.initState();
-    logger.fine('$widget.objectId initState()');
 
     (() async {
       await _renderer.initialize();
@@ -48,12 +43,11 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
 
   @override
   void dispose() {
-    logger.fine('$widget.objectId dispose()');
-    VisibilityDetectorController.instance.forget(_keyForVisibilityDetector);
+    VisibilityDetectorController.instance.forget(widget.key!);
     // report that instance is disposing
     // if the track is disposed first we can't emit event
     widget.track.events.emit(TrackVisibilityUpdatedEvent(
-      rendererId: widget.objectId,
+      rendererId: widget.key!.toString(),
       track: widget.track,
       info: null,
     ));
@@ -64,12 +58,13 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
   }
 
   Future<void> _attach() async {
-    logger.fine('[VideoTrackRenderer] attached to ${widget.track.objectId}');
     _renderer.srcObject = widget.track.mediaStream;
     await _listener?.dispose();
     _listener = widget.track.createListener()
       ..on<TrackStreamUpdatedEvent>((event) {
-        _renderer.srcObject = event.stream;
+        if (mounted) {
+          _renderer.srcObject = event.stream;
+        }
       });
   }
 
@@ -88,11 +83,11 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
   Widget build(BuildContext context) => !_rendererReady
       ? Container()
       : VisibilityDetector(
-          key: _keyForVisibilityDetector,
+          key: widget.key!,
           // emit event when visibility updates
           onVisibilityChanged: (VisibilityInfo info) =>
               widget.track.events.emit(TrackVisibilityUpdatedEvent(
-            rendererId: widget.objectId,
+            rendererId: widget.key!.toString(),
             track: widget.track,
             info: info,
           )),
