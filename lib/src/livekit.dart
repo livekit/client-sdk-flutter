37f2def1b2e74c50df57a1e65271456fd4cb30ad
cloudwebrtc <duanweiwei1982@gmail.com>
2022-07-11 11:55:51 +0800

    Deprecated the top connect method.

--- lib/src/livekit.dart
+++ lib/src/livekit.dart
@@ -9,6 +9,8 @@ class LiveKitClient {
   /// Convenience method for connecting to a LiveKit server.
   /// Returns a [Room] upon a successful connect or throws when it fails.
   /// Alternatively, it is possible to instantiate [Room] and call [Room.connect] directly.
+  @Deprecated(
+      'Use `Room.connect` instead, This method is deprecated above Protocol v8.')
   static Future<Room> connect(
     String url,
     String token, {
