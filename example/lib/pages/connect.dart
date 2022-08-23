1804c4a2103702b9bc458cd5b49cc4a91aa072d0
cloudwebrtc <duanweiwei1982@gmail.com>
2022-08-23 10:56:16 +0800

    Removed hardware settings dialog, added it to experimental branch.

--- example/lib/pages/connect.dart
+++ example/lib/pages/connect.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:livekit_client/livekit_client.dart';
-import 'package:livekit_example/widgets/settings.dart';
 import 'package:livekit_example/widgets/text_field.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -74,13 +73,6 @@ class _ConnectPageState extends State<ConnectPage> {
     await prefs.setBool(_storeKeyAdaptiveStream, _adaptiveStream);
     await prefs.setBool(_storeKeyDynacast, _dynacast);
     await prefs.setBool(_storeKeyFastConnect, _fastConnect);
-  }
-
-  void _openSettings() async {
-    await showDialog<void>(
-      context: context,
-      builder: (context) => SettingsDialog(),
-    );
   }
 
   Future<void> _connect(BuildContext ctx) async {
@@ -167,131 +159,116 @@ class _ConnectPageState extends State<ConnectPage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-          body: Stack(children: <Widget>[
-        Align(
-          alignment: const Alignment(0, 0),
-          child: Container(
-            alignment: Alignment.center,
-            child: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                constraints: const BoxConstraints(maxWidth: 400),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 70),
-                      child: SvgPicture.asset(
-                        'images/logo-dark.svg',
-                      ),
+        body: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              constraints: const BoxConstraints(maxWidth: 400),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 70),
+                    child: SvgPicture.asset(
+                      'images/logo-dark.svg',
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 25),
-                      child: LKTextField(
-                        label: 'Server URL',
-                        ctrl: _uriCtrl,
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: LKTextField(
+                      label: 'Server URL',
+                      ctrl: _uriCtrl,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 25),
-                      child: LKTextField(
-                        label: 'Token',
-                        ctrl: _tokenCtrl,
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: LKTextField(
+                      label: 'Token',
+                      ctrl: _tokenCtrl,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Simulcast'),
-                          Switch(
-                            value: _simulcast,
-                            onChanged: (value) => _setSimulcast(value),
-                          ),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Simulcast'),
+                        Switch(
+                          value: _simulcast,
+                          onChanged: (value) => _setSimulcast(value),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Adaptive Stream'),
-                          Switch(
-                            value: _adaptiveStream,
-                            onChanged: (value) => _setAdaptiveStream(value),
-                          ),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Adaptive Stream'),
+                        Switch(
+                          value: _adaptiveStream,
+                          onChanged: (value) => _setAdaptiveStream(value),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Fast Connect'),
-                          Switch(
-                            value: _fastConnect,
-                            onChanged: (value) => _setFastConnect(value),
-                          ),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Fast Connect'),
+                        Switch(
+                          value: _fastConnect,
+                          onChanged: (value) => _setFastConnect(value),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 25),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Dynacast'),
-                          Switch(
-                            value: _dynacast,
-                            onChanged: (value) => _setDynacast(value),
-                          ),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Dynacast'),
+                        Switch(
+                          value: _dynacast,
+                          onChanged: (value) => _setDynacast(value),
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      onPressed: _busy ? null : () => _connect(context),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          if (_busy)
-                            const Padding(
-                              padding: EdgeInsets.only(right: 10),
-                              child: SizedBox(
-                                height: 15,
-                                width: 15,
-                                child: CircularProgressIndicator(
-                                  color: Colors.white,
-                                  strokeWidth: 2,
-                                ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _busy ? null : () => _connect(context),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (_busy)
+                          const Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: SizedBox(
+                              height: 15,
+                              width: 15,
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                                strokeWidth: 2,
                               ),
                             ),
-                          const Text('CONNECT'),
-                        ],
-                      ),
+                          ),
+                        const Text('CONNECT'),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
         ),
-        Positioned(
-          bottom: 12.0,
-          right: 12.0,
-          child: TextButton(
-            onPressed: _openSettings,
-            child: const Text(
-              'Settings',
-              style: TextStyle(fontSize: 16.0, color: Colors.white),
-            ),
-          ),
-        ),
-      ]));
+      );
 }
