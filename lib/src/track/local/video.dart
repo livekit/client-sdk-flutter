021038d239144768ab572e80f8a89041ddd27832
cloudwebrtc <duanweiwei1982@gmail.com>
2023-02-13 13:41:17 +0800

    feat: support fast switch camera for LocalVideoTrack.

--- lib/src/track/local/video.dart
+++ lib/src/track/local/video.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
+import 'package:livekit_client/src/support/platform.dart';
 
 import '../../logger.dart';
 import '../../proto/livekit_models.pb.dart' as lk_models;
@@ -116,10 +117,15 @@ extension LocalVideoTrackExt on LocalVideoTrack {
     );
   }
 
-  Future<void> switchCamera(String deviceId) async {
+  Future<void> switchCamera(String deviceId, {bool fastSwitch = false}) async {
     final options = currentOptions;
     if (options is! CameraCaptureOptions) {
       logger.warning('Not a camera track');
+      return;
+    }
+
+    if (fastSwitch) {
+      await rtc.Helper.switchCamera(mediaStreamTrack, deviceId, mediaStream);
       return;
     }
 
