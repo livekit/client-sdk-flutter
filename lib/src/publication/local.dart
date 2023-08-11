53f89d6893e60196095dbca7bf530f890e89845e
duanweiwei1982@gmail.com <duanweiwei1982@gmail.com>
2023-08-11 10:28:41 +0800

    dart format.

--- lib/src/publication/local.dart
+++ lib/src/publication/local.dart
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 import '../participant/local.dart';
 import '../proto/livekit_models.pb.dart' as lk_models;
 import '../proto/livekit_rtc.pb.dart' as lk_rtc;
