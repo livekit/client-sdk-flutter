4815aef23cf5f43731ee13739cc9ac6b310606e0
cloudwebrtc <duanweiwei1982@gmail.com>
2023-09-14 23:47:19 +0800

    support setMungedSDP.

--- lib/src/participant/local.dart
+++ lib/src/participant/local.dart
@@ -15,6 +15,8 @@
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
+import 'package:livekit_client/livekit_client.dart';
+import 'package:livekit_client/src/core/transport.dart';
 import 'package:meta/meta.dart';
 
 import '../core/engine.dart';
@@ -261,6 +263,19 @@ class LocalParticipant extends Participant<LocalTrackPublication> {
       parameters.degradationPreference =
           rtc.RTCDegradationPreference.MAINTAIN_RESOLUTION;
       await sender.setParameters(parameters);
+    }
+
+    if (kIsWeb &&
+        lkBrowser() == BrowserType.firefox &&
+        track.kind == TrackType.AUDIO) {
+      //TOOD:
+    } else if (isSVCCodec(publishOptions.videoCodec) &&
+        encodings?.first.maxBitrate != null) {
+      room.engine.publisher?.setTrackBitrateInfo(TrackBitrateInfo(
+          cid: track.getCid(),
+          transceiver: track.transceiver,
+          codec: publishOptions.videoCodec,
+          maxbr: encodings![0].maxBitrate! ~/ 1000));
     }
 
     await room.engine.negotiate();
