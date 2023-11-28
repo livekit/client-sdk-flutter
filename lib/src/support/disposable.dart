64cbccc718490cbb67df84c4556d554d252a5f40
cloudwebrtc <duanweiwei1982@gmail.com>
2023-11-28 12:33:31 +0800

    add checkIfDesposed.

--- lib/src/support/disposable.dart
+++ lib/src/support/disposable.dart
@@ -51,6 +51,12 @@ mixin _Disposer {
       return false;
     }
   }
+
+  void checkIfDisposed() {
+    if (_isDisposed) {
+      throw Exception('[$objectId] already disposed');
+    }
+  }
 }
 
 abstract class Disposable with _Disposer {
