345b6c49d024e02e70d1969bfc7ef7437068c74d
cloudwebrtc <duanweiwei1982@gmail.com>
2024-04-04 08:19:10 +0800

    fix deprecated_member_use_from_same_package warn.

--- lib/src/participant/local.dart
+++ lib/src/participant/local.dart
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// ignore_for_file: deprecated_member_use_from_same_package
+
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
