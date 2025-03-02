86b063103e1b523ae8314b775216647ae6c9063a
cloudwebrtc <duanweiwei1982@gmail.com>
2025-03-02 22:40:49 +0800

    fix.

--- lib/src/track/audio_visualizer.dart
+++ lib/src/track/audio_visualizer.dart
@@ -3,8 +3,8 @@ import '../events.dart' show AudioVisualizerEvent;
 import '../managers/event.dart' show EventsEmittable;
 import 'local/local.dart' show AudioTrack;
 
-import 'visualizer_native.dart'
-    if (dart.library.js_interop) 'visualizer_web.dart';
+import 'audio_visualizer_native.dart'
+    if (dart.library.js_interop) 'audio_visualizer_web.dart';
 
 class AudioVisualizerOptions {
   final bool centeredBands;
