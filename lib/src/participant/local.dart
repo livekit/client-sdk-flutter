33c38981466c0a9780b1c0be27332ca9319bc85b
cloudwebrtc <duanweiwei1982@gmail.com>
2025-04-04 07:49:08 +0800

    fix: bug for fast track publish.

--- lib/src/participant/local.dart
+++ lib/src/participant/local.dart
@@ -207,8 +207,11 @@ class LocalParticipant extends Participant<LocalTrackPublication> {
 
     if (room.engine.enabledPublishCodecs?.isNotEmpty ?? false) {
       // fallback to a supported codec if it is not supported
-      if (!room.engine.enabledPublishCodecs!.any((c) =>
-          publishOptions?.videoCodec == mimeTypeToVideoCodecString(c.mime))) {
+      if (!room.engine.enabledPublishCodecs!
+          .where((c) => c.mime.startsWith('video/'))
+          .any((c) =>
+              publishOptions?.videoCodec ==
+              mimeTypeToVideoCodecString(c.mime))) {
         publishOptions = publishOptions.copyWith(
           videoCodec: mimeTypeToVideoCodecString(
                   room.engine.enabledPublishCodecs![0].mime)
