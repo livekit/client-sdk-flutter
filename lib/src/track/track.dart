60a72be716d88229d859d330ef728d888eb7558f
holzgeist <holzgeist@users.noreply.github.com>
2025-04-03 04:04:43 +0200

    fix: don't close media stream tracks for web (#722)
    * fix: don't close media stream tracks for web
    
    * lint: run import sorter

--- lib/src/track/track.dart
+++ lib/src/track/track.dart
@@ -14,6 +14,8 @@
 
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
 import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
@@ -131,7 +133,9 @@ abstract class Track extends DisposableChangeNotifier
 
     logger.fine('$objectId.stop()');
 
-    await mediaStreamTrack.stop();
+    if (!kIsWeb) {
+      await mediaStreamTrack.stop();
+    }
 
     _active = false;
     return true;
