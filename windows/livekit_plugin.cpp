76a1849fee502b8627b3b4ea5f8576714fc7f9df
cloudwebrtc <duanweiwei1982@gmail.com>
2025-06-24 12:06:57 +0800

    update.

--- windows/livekit_plugin.cpp
+++ windows/livekit_plugin.cpp
@@ -31,12 +31,39 @@
 #include <sstream>
 
 #include "audio_visualizer.h"
+#include "task_runner_windows.h"
 
-namespace {
+namespace livekit_client_plugin {
 
 class VisualizerSink : public libwebrtc::AudioTrackSink {
 public:
-  VisualizerSink() {}
+  VisualizerSink(BinaryMessenger *messenger, std::string event_channel_name)
+      : channel_(std::make_unique<EventChannel>(
+            messenger, event_channel_name,
+            &flutter::StandardMethodCodec::GetInstance())) {
+    task_runner_ = std::make_unique<livekit_client_plugin::TaskRunnerWindows>();
+    auto handler = std::make_unique<
+        flutter::StreamHandlerFunctions<EncodableValue>>(
+        [&](const EncodableValue *arguments,
+            std::unique_ptr<flutter::EventSink<EncodableValue>> &&events)
+            -> std::unique_ptr<flutter::StreamHandlerError<EncodableValue>> {
+          sink_ = std::move(events);
+          std::weak_ptr<EventSink> weak_sink = sink_;
+          for (auto &event : event_queue_) {
+            PostEvent(event);
+          }
+          event_queue_.clear();
+          on_listen_called_ = true;
+          return nullptr;
+        },
+        [&](const EncodableValue *arguments)
+            -> std::unique_ptr<flutter::StreamHandlerError<EncodableValue>> {
+          on_listen_called_ = false;
+          return nullptr;
+        });
+
+    channel_->SetStreamHandler(std::move(handler));
+  }
   ~VisualizerSink() override {}
 
 public:
@@ -52,18 +79,51 @@ public:
         std::cout << static_cast<int>(band) << ", ";
       }
       std::cout << std::endl;
+
+      // Post the processed data to the event sink
+      EncodableList bands_list = EncodableList(bands.begin(), bands.end());
+      Success(EncodableValue(bands_list));
+    }
+  }
+
+  void Success(const EncodableValue &event, bool cache_event = true) {
+    if (on_listen_called_) {
+      PostEvent(event);
+    } else {
+      if (cache_event) {
+        event_queue_.push_back(event);
+      }
+    }
+  }
+
+  void PostEvent(const EncodableValue &event) {
+    if (task_runner_) {
+      std::weak_ptr<EventSink> weak_sink = sink_;
+      task_runner_->EnqueueTask([weak_sink, event]() {
+        auto sink = weak_sink.lock();
+        if (sink) {
+          sink->Success(event);
+        }
+      });
+    } else {
+      sink_->Success(event);
     }
   }
 
 private:
   AudioVisualizer audio_visualizer_;
+  std::unique_ptr<livekit_client_plugin::TaskRunnerWindows> task_runner_;
+  std::unique_ptr<flutter::EventChannel<EncodableValue>> channel_;
+  std::shared_ptr<flutter::EventSink<flutter::EncodableValue>> sink_;
+  std::list<EncodableValue> event_queue_;
+  bool on_listen_called_ = false;
 };
 
 class LiveKitPlugin : public flutter::Plugin {
 public:
   static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  LiveKitPlugin();
+  LiveKitPlugin(BinaryMessenger *messenger);
 
   virtual ~LiveKitPlugin();
 
@@ -75,7 +135,8 @@ private:
 
 private:
   flutter_webrtc_plugin::FlutterWebRTC *webrtc_instance_ = nullptr;
-  VisualizerSink audio_sink_; // Example audio sink instance
+  std::unordered_map<std::string, std::unique_ptr<VisualizerSink>> visualizers_;
+  BinaryMessenger *messenger_ = nullptr;
 };
 
 // static
@@ -86,7 +147,7 @@ void LiveKitPlugin::RegisterWithRegistrar(
           registrar->messenger(), "livekit_client",
           &flutter::StandardMethodCodec::GetInstance());
 
-  auto plugin = std::make_unique<LiveKitPlugin>();
+  auto plugin = std::make_unique<LiveKitPlugin>(registrar->messenger());
 
   channel->SetMethodCallHandler(
       [plugin_pointer = plugin.get()](const auto &call, auto result) {
@@ -96,7 +157,8 @@ void LiveKitPlugin::RegisterWithRegistrar(
   registrar->AddPlugin(std::move(plugin));
 }
 
-LiveKitPlugin::LiveKitPlugin() {
+LiveKitPlugin::LiveKitPlugin(BinaryMessenger *messenger)
+    : messenger_(messenger) {
   webrtc_instance_ = FlutterWebRTCPluginSharedInstance();
 }
 
@@ -127,8 +189,15 @@ void LiveKitPlugin::HandleMethodCall(
       result->Error("Track Not Found", "No media track found for the given ID");
       return;
     }
+    std::ostringstream oss;
+    oss << "io.livekit.audio.visualizer/eventChannel-" << trackId << "-"
+        << visualizerId;
 
-    media_track->AddSink(&audio_sink_);
+    auto sink = std::make_unique<VisualizerSink>(messenger_, oss.str());
+
+    media_track->AddSink(sink.get());
+
+    visualizers_[visualizerId] = std::move(sink);
 
     std::cout << "Starting visualizer for trackId: " << trackId
               << ", visualizerId: " << visualizerId
@@ -156,7 +225,15 @@ void LiveKitPlugin::HandleMethodCall(
       return;
     }
 
-    media_track->RemoveSink(&audio_sink_);
+    auto it = visualizers_.find(visualizerId);
+    if (it != visualizers_.end()) {
+      media_track->RemoveSink(it->second.get());
+      visualizers_.erase(it);
+    } else {
+      result->Error("Visualizer Not Found",
+                    "No visualizer found for the given visualizerId");
+      return;
+    }
 
     result->Success(flutter::EncodableValue("Visualizer stopped"));
   } else {
@@ -164,11 +241,11 @@ void LiveKitPlugin::HandleMethodCall(
   }
 }
 
-} // namespace
+} // namespace livekit_client_plugin
 
 void LiveKitPluginRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar) {
-  LiveKitPlugin::RegisterWithRegistrar(
+  livekit_client_plugin::LiveKitPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
           ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
 }
