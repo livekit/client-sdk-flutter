f8db52b3fcc15ecf5ba6d1e21ec0bd40340e79c8
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-10-23 14:40:48 +0700

    squash fixes

--- lib/src/core/room.dart
+++ lib/src/core/room.dart
@@ -38,6 +38,7 @@ import '../participant/participant.dart';
 import '../participant/remote.dart';
 import '../proto/livekit_models.pb.dart' as lk_models;
 import '../proto/livekit_rtc.pb.dart' as lk_rtc;
+import '../publication/remote.dart';
 import '../support/disposable.dart';
 import '../support/platform.dart';
 import '../support/region_url_provider.dart';
@@ -636,15 +637,21 @@ class Room extends DisposableChangeNotifier with EventsEmittable<RoomEvent> {
     return null;
   }
 
-  Future<RemoteParticipant> _getOrCreateRemoteParticipant(String identity, lk_models.ParticipantInfo? info) async {
+  Future<ParticipantCreationResult> _getOrCreateRemoteParticipant(
+      String identity, lk_models.ParticipantInfo? info) async {
     RemoteParticipant? participant = _remoteParticipants[identity];
     if (participant != null) {
       if (info != null) {
         await participant.updateFromInfo(info);
       }
-      return participant;
+      // Return existing participant with no new publications
+      return ParticipantCreationResult(
+        participant: participant,
+        newPublications: const [],
+      );
     }
 
+    ParticipantCreationResult result;
     if (info == null) {
       logger.warning('RemoteParticipant.info is null identity: $identity');
       participant = RemoteParticipant(
@@ -653,16 +660,20 @@ class Room extends DisposableChangeNotifier with EventsEmittable<RoomEvent> {
         identity: identity,
         name: '',
       );
+      result = ParticipantCreationResult(
+        participant: participant,
+        newPublications: const [],
+      );
     } else {
-      participant = await RemoteParticipant.createFromInfo(
+      result = await RemoteParticipant.createFromInfo(
         room: this,
         info: info,
       );
     }
 
-    _remoteParticipants[identity] = participant;
-    _sidToIdentity[participant.sid] = identity;
-    return participant;
+    _remoteParticipants[result.participant.identity] = result.participant;
+    _sidToIdentity[result.participant.sid] = result.participant.identity;
+    return result;
   }
 
   Future<void> _onParticipantUpdateEvent(List<lk_models.ParticipantInfo> updates) async {
@@ -689,14 +700,22 @@ class Room extends DisposableChangeNotifier with EventsEmittable<RoomEvent> {
         continue;
       }
 
-      final participant = await _getOrCreateRemoteParticipant(info.identity, info);
+      final result = await _getOrCreateRemoteParticipant(info.identity, info);
 
       if (isNew) {
         hasChanged = true;
-        // fire connected event
-        emitWhenConnected(ParticipantConnectedEvent(participant: participant));
+        // Emit connected event
+        emitWhenConnected(ParticipantConnectedEvent(participant: result.participant));
+        // Emit TrackPublishedEvent for each new track
+        for (final pub in result.newPublications) {
+          [events].emit(TrackPublishedEvent(
+            participant: result.participant,
+            publication: pub,
+          ));
+        }
+        _sidToIdentity[info.sid] = info.identity;
       } else {
-        final wasUpdated = await participant.updateFromInfo(info);
+        final wasUpdated = await result.participant.updateFromInfo(info);
         if (wasUpdated) {
           _sidToIdentity[info.sid] = info.identity;
         }
