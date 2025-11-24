cf16beef5aa4c9411495616a1ff45e70830c03e7
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2025-11-24 19:56:21 +0100

    Regenerate files

--- lib/src/token_source/jwt.g.dart
+++ lib/src/token_source/jwt.g.dart
@@ -6,7 +6,8 @@ part of 'jwt.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-LiveKitVideoGrant _$LiveKitVideoGrantFromJson(Map<String, dynamic> json) => LiveKitVideoGrant(
+LiveKitVideoGrant _$LiveKitVideoGrantFromJson(Map<String, dynamic> json) =>
+    LiveKitVideoGrant(
       room: json['room'] as String?,
       roomCreate: json['room_create'] as bool?,
       roomJoin: json['room_join'] as bool?,
@@ -16,12 +17,15 @@ LiveKitVideoGrant _$LiveKitVideoGrantFromJson(Map<String, dynamic> json) => Live
       canPublish: json['can_publish'] as bool?,
       canSubscribe: json['can_subscribe'] as bool?,
       canPublishData: json['can_publish_data'] as bool?,
-      canPublishSources: (json['can_publish_sources'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      canPublishSources: (json['can_publish_sources'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
       hidden: json['hidden'] as bool?,
       recorder: json['recorder'] as bool?,
     );
 
-Map<String, dynamic> _$LiveKitVideoGrantToJson(LiveKitVideoGrant instance) => <String, dynamic>{
+Map<String, dynamic> _$LiveKitVideoGrantToJson(LiveKitVideoGrant instance) =>
+    <String, dynamic>{
       if (instance.room case final value?) 'room': value,
       if (instance.roomCreate case final value?) 'room_create': value,
       if (instance.roomJoin case final value?) 'room_join': value,
@@ -31,7 +35,8 @@ Map<String, dynamic> _$LiveKitVideoGrantToJson(LiveKitVideoGrant instance) => <S
       if (instance.canPublish case final value?) 'can_publish': value,
       if (instance.canSubscribe case final value?) 'can_subscribe': value,
       if (instance.canPublishData case final value?) 'can_publish_data': value,
-      if (instance.canPublishSources case final value?) 'can_publish_sources': value,
+      if (instance.canPublishSources case final value?)
+        'can_publish_sources': value,
       if (instance.hidden case final value?) 'hidden': value,
       if (instance.recorder case final value?) 'recorder': value,
     };
--- lib/src/types/attribute_typings.g.dart
+++ lib/src/types/attribute_typings.g.dart
@@ -6,22 +6,31 @@ part of 'attribute_typings.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-AgentAttributes _$AgentAttributesFromJson(Map<String, dynamic> json) => AgentAttributes(
-      lkAgentInputs:
-          (json['lk.agent.inputs'] as List<dynamic>?)?.map((e) => $enumDecode(_$AgentInputEnumMap, e)).toList(),
-      lkAgentOutputs:
-          (json['lk.agent.outputs'] as List<dynamic>?)?.map((e) => $enumDecode(_$AgentOutputEnumMap, e)).toList(),
-      lkAgentState: $enumDecodeNullable(_$AgentStateEnumMap, json['lk.agent.state']),
+AgentAttributes _$AgentAttributesFromJson(Map<String, dynamic> json) =>
+    AgentAttributes(
+      lkAgentInputs: (json['lk.agent.inputs'] as List<dynamic>?)
+          ?.map((e) => $enumDecode(_$AgentInputEnumMap, e))
+          .toList(),
+      lkAgentOutputs: (json['lk.agent.outputs'] as List<dynamic>?)
+          ?.map((e) => $enumDecode(_$AgentOutputEnumMap, e))
+          .toList(),
+      lkAgentState:
+          $enumDecodeNullable(_$AgentStateEnumMap, json['lk.agent.state']),
       lkPublishOnBehalf: json['lk.publish_on_behalf'] as String?,
     );
 
-Map<String, dynamic> _$AgentAttributesToJson(AgentAttributes instance) => <String, dynamic>{
-      if (instance.lkAgentInputs?.map((e) => _$AgentInputEnumMap[e]!).toList() case final value?)
+Map<String, dynamic> _$AgentAttributesToJson(AgentAttributes instance) =>
+    <String, dynamic>{
+      if (instance.lkAgentInputs?.map((e) => _$AgentInputEnumMap[e]!).toList()
+          case final value?)
         'lk.agent.inputs': value,
-      if (instance.lkAgentOutputs?.map((e) => _$AgentOutputEnumMap[e]!).toList() case final value?)
+      if (instance.lkAgentOutputs?.map((e) => _$AgentOutputEnumMap[e]!).toList()
+          case final value?)
         'lk.agent.outputs': value,
-      if (_$AgentStateEnumMap[instance.lkAgentState] case final value?) 'lk.agent.state': value,
-      if (instance.lkPublishOnBehalf case final value?) 'lk.publish_on_behalf': value,
+      if (_$AgentStateEnumMap[instance.lkAgentState] case final value?)
+        'lk.agent.state': value,
+      if (instance.lkPublishOnBehalf case final value?)
+        'lk.publish_on_behalf': value,
     };
 
 const _$AgentInputEnumMap = {
@@ -43,14 +52,20 @@ const _$AgentStateEnumMap = {
   AgentState.thinking: 'thinking',
 };
 
-TranscriptionAttributes _$TranscriptionAttributesFromJson(Map<String, dynamic> json) => TranscriptionAttributes(
+TranscriptionAttributes _$TranscriptionAttributesFromJson(
+        Map<String, dynamic> json) =>
+    TranscriptionAttributes(
       lkSegmentId: json['lk.segment_id'] as String?,
       lkTranscribedTrackId: json['lk.transcribed_track_id'] as String?,
       lkTranscriptionFinal: json['lk.transcription_final'] as bool?,
     );
 
-Map<String, dynamic> _$TranscriptionAttributesToJson(TranscriptionAttributes instance) => <String, dynamic>{
+Map<String, dynamic> _$TranscriptionAttributesToJson(
+        TranscriptionAttributes instance) =>
+    <String, dynamic>{
       if (instance.lkSegmentId case final value?) 'lk.segment_id': value,
-      if (instance.lkTranscribedTrackId case final value?) 'lk.transcribed_track_id': value,
-      if (instance.lkTranscriptionFinal case final value?) 'lk.transcription_final': value,
+      if (instance.lkTranscribedTrackId case final value?)
+        'lk.transcribed_track_id': value,
+      if (instance.lkTranscriptionFinal case final value?)
+        'lk.transcription_final': value,
     };
