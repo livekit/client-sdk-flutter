7b9f0c1a32545690681c2c64a7174b045fe1541a
Aditya Kaldhone <adityakaldhone1357@gmail.com>
2026-02-05 23:28:47 +0530

    fixed ../../.pub-cache/hosted/pub.dev/livekit_client-2.6.2/lib/src/track/local/video.dart:178:4: Error: Undefined name 'internal' this issue

--- lib/src/track/local/video.dart
+++ lib/src/track/local/video.dart
@@ -16,6 +16,7 @@ import 'package:flutter/foundation.dart';
 
 import 'package:collection/collection.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
+import 'package:meta/meta.dart';
 
 import '../../events.dart';
 import '../../exceptions.dart';
