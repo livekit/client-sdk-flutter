f301f990970f01b94f84e894d71a8411d6ca766e
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2026-05-25 19:32:41 +0900

    Simplify video render mode selection

--- lib/src/widgets/video_track_renderer.dart
+++ lib/src/widgets/video_track_renderer.dart
@@ -100,9 +100,6 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
   }
 
   Future<rtc.VideoRenderer> _initializeRenderer() async {
-    if (_shouldUsePlatformView) {
-      return Null as Future<rtc.VideoRenderer>;
-    }
     if (_renderer == null) {
       _renderer = rtc.RTCVideoRenderer();
       await _renderer!.initialize();
@@ -184,10 +181,21 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
     _renderer?.onResize = () {
       if (mounted) {
         setState(() {
-          _aspectRatio = (_renderer as rtc.RTCVideoRenderer?)?.videoValue.aspectRatio;
+          _aspectRatio = _rendererAspectRatio;
         });
       }
     };
+  }
+
+  double? get _rendererAspectRatio {
+    final renderer = _renderer;
+    if (renderer is rtc.RTCVideoRenderer) {
+      return renderer.videoValue.aspectRatio;
+    }
+    if (renderer is rtc.RTCVideoPlatformViewController) {
+      return renderer.value.aspectRatio;
+    }
+    return null;
   }
 
   @override
@@ -224,8 +232,8 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
           },
         );
 
-  Widget _videoRendererView() {
-    if (_shouldUsePlatformView) {
+  Widget _videoRendererView({required bool usePlatformView}) {
+    if (usePlatformView) {
       return rtc.RTCVideoPlatFormView(
         mirror: _shouldMirror(),
         objectFit: widget.fit.toRTCType(),
@@ -244,10 +252,12 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
     );
   }
 
-  Widget _videoViewForNative() => FutureBuilder(
-      future: _initializeRenderer(),
+  Widget _videoViewForNative() {
+    final usePlatformView = _shouldUsePlatformView;
+    return FutureBuilder(
+      future: usePlatformView ? null : _initializeRenderer(),
       builder: (context, snapshot) {
-        if ((snapshot.hasData && _renderer != null) || _shouldUsePlatformView) {
+        if ((snapshot.hasData && _renderer != null) || usePlatformView) {
           return Builder(
             key: _internalKey,
             builder: (ctx) {
@@ -257,7 +267,7 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
               });
 
               if (!lkPlatformIsMobile() || widget.track is! LocalVideoTrack) {
-                return _videoRendererView();
+                return _videoRendererView(usePlatformView: usePlatformView);
               }
               return LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints constraints) {
@@ -269,7 +279,7 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
                       }
                     },
                     onTapDown: (TapDownDetails details) => onViewFinderTap(details, constraints),
-                    child: _videoRendererView(),
+                    child: _videoRendererView(usePlatformView: usePlatformView),
                   );
                 },
               );
@@ -277,7 +287,9 @@ class _VideoTrackRendererState extends State<VideoTrackRenderer> {
           );
         }
         return Container();
-      });
+      },
+    );
+  }
 
   // FutureBuilder will cause flickering for flutter web. so using
   // different rendering methods for web and native.
