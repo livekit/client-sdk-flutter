23762d4f112dfefa2ec7067ea89e0459ad4f7f5c
hiroshihorie <hiroshihorie@users.noreply.github.com>
2026-07-24 08:36:24 +0000

    deploy: fdcedaa0803b6c5a530ff975deb7bb816364553c

--- main.dart.js
+++ main.dart.js
@@ -37044,7 +37044,7 @@ $S:2}
 A.aYa.prototype={
 $1(a){var s=A.e7().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
 $S:63}
 A.Ys.prototype={
 gt(a){var s=this.a
