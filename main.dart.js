62d68c846adef48439b9c9b2d63837e85b237772
xianshijing-lk <xianshijing-lk@users.noreply.github.com>
2026-09-23 01:34:42 +0000

    deploy: 41696bd0f09e889488c8fda97ac1496a4ad228d3

--- main.dart.js
+++ main.dart.js
@@ -37715,7 +37715,7 @@ $S:2}
 A.aZR.prototype={
 $1(a){var s=A.dV().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/06a2e2a110089dff50fe635cffd2a61e1b24fbcd/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/af7e796e161ae0bb1ff0758c71a7105418bd9ded/":s)+a},
 $S:53}
 A.Za.prototype={
 gt(a){var s=this.a
